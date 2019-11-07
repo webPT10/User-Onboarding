b55c7ce0b93cd0e20cfcdf3d546979c115e6fb3e
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-11-07 11:02:49 -0800

    created React App, added formik yup axios, created Form.js imported to App

---
+++ eric-ferguson/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import Form from './Components/Form';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Form />
+      </header>
+    </div>
+  );
+}
+
+export default App;
